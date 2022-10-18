2c0a746c887eac0915043c4d58ce647726b1e68c
Gtindi <tindigabriel@gmail.com>
2022-10-18 18:05:53 +0300

    Repository updated

--- main.h
+++ main.h
@@ -1,73 +1,52 @@
-#ifndef _MAIN_
-#define _MAIN_
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdlib.h>
 #include <stdarg.h>
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
 
-#define STR_TO_INT(val) (val - 48)
-#define INT_TO_STR(val) (val + 48)
 /**
- * struct ops - Struct ops
- * @c: The character associated to this op
- * @op: A callback function to perform the print
- * operation.
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
  */
-typedef struct ops
+typedef struct printHandler
 {
 	char c;
-	int (*op)(int, char *, va_list);
-} op_t;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+char *convert(unsigned long int num, int base, int lowercase);
 int _printf(const char *format, ...);
-int (*get_print_function(char *format))(int, char *, va_list);
+int (*get_print(char s))(va_list, flags_t *);
+int get_flag(char s, flags_t *f);
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
 int _putchar(char c);
-int print_char(int n, char *format, va_list args);
-int print_string(int n, char *format, va_list args);
-int print_int(int n, char *format, va_list args);
-int print_float(int n, char *format, va_list args);
-int print_binary(int n, char *format, va_list args);
-int print_hex(int n, char *format, va_list args);
-int print_octal(int n, char *format, va_list args);
-int print_unsigned_int(int n, char *format, va_list args);
-int print_address(int n, char *format, va_list args);
-int _is_alpha(char c);
-int _is_specifier(char c);
-unsigned int compute_places(unsigned int n, int step);
-int _strlen(char *s);
-char *rot13(char *str);
-char *_reverse_string(char *str);
-int print_reverse(int n, char *format, va_list args);
-int print_rot13(int n, char *format, va_list args);
-int print_custom_specifier(int n, char *format, va_list args);
-int _is_digit(char c);
-int _atoi(char *s);
-int _pow_recursion(int x, int y);
-int left_align(char *format, int n);
-int right_align(char *format, int n);
-int pad_zero(char *format, int n);
-int count_int(int n);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_strcpy(char *dest, char *src);
-int get_max_width(char *format);
-int get_min_width(char *format);
-int has_precision(char *str);
-int print_string_width(char *format, char *str);
-int print_integer_width(char *format, int n);
-char *to_octal(unsigned int n);
-char *to_hex(long unsigned int n);
-char *_tolower(char *str);
-char get_hex_code(int n);
-int _indexof(char *str, char c);
-int print_digit(int n, int is_int_min);
-/**
- * struct hex_code - Struct hex_code
- * @c: The character associated to this code
- * @code: The digit for this char
- * operation.
- */
-typedef struct hex_code
-{
-	char c;
-	int code;
-} hex_code_t;
 int _puts(char *str);
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+int print_address(va_list l, flags_t *f);
+int print_percent(va_list l, flags_t *f);
 #endif
